88ba98f3053d5fe4886b8e19aa1d96e1287f1b45
Manuel Pérez <111928781+ManuelAlfonsoP@users.noreply.github.com>
2025-09-09 03:00:32 +0200

    Parcial 1

---
+++ PARCIALES/PARCIAL_1/procesador_frases.php
@@ -0,0 +1,7 @@
+<?php
+include 'operaciones_cadenas.php'
+
+$frases = ["las manzanas me gustan mucho","aaa bbb ccc ddd fff aaa bbb", "no se me ocurre que poner en este arreglo", "me costo algo la parte 1"];
+
+
+?>
